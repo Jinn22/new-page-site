1a054a15f8f86805fcd485e9cefcb29b127eb586
Jeremy C. Chan <jeremy.c.chan@accenture.com>
2018-09-19 15:31:26 +0100

    v2

--- ConnectDB.java
+++ ConnectDB.java
@@ -4,7 +4,20 @@ public class ConnectDB {
    static final String DB_URL = 'https://online-database.client.com:12345/api' 
    
    public static void main(String[] args) {
-   
+    try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver"); 
+
+            //STEP 3: Open a connection
+            //Database credentials
+            String USER = 'ken.yang'
+            String PASSWORD = 'password12345'
+            System.out.println("Connecting to database...");
+            Connection conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
 
 
