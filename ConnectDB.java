da49466c06c4ef3cf93f68122b7b5ffe61d37b55
Jeremy C. Chan <jeremy.c.chan@accenture.com>
2018-09-19 15:31:42 +0100

    v3

--- ConnectDB.java
+++ ConnectDB.java
@@ -14,6 +14,16 @@ public class ConnectDB {
             String PASSWORD = 'password12345'
             System.out.println("Connecting to database...");
             Connection conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            Statement stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT id, name, age FROM Employees";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            ...
         }catch(Exception e){
             e.printStackTrace();
         }
